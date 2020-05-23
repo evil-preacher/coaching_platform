8690f241dad7db0d350c3d8780927a71f7543c3e
Paul Pukhtiy <paul.pukhtiy@gmail.com>
2020-05-24 00:55:56 +0600

    new: delete assets folder/add bootstrap and new main page

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -11,6 +11,7 @@ require("channels")
 import "core-js/stable"
 import "regenerator-runtime/runtime"
 import "stylesheets/application"
+import './bootstrap_custom.js'
 
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
