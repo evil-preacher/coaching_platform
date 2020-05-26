a0bf3162e91386c91ce37a152ff5b04e677b312f
Paul Pukhtiy <paul.pukhtiy@gmail.com>
2020-05-26 21:36:21 +0600

    new: main page styling

--- app/frontend/packs/application.js
+++ app/frontend/packs/application.js
@@ -1,8 +1,3 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
@@ -10,8 +5,11 @@ require("channels")
 
 import "core-js/stable"
 import "regenerator-runtime/runtime"
-import "stylesheets/application"
+import "../stylesheets/application"
 import './bootstrap_custom.js'
 
 const images = require.context('../images', true)
 const imagePath = (name) => images(name, true)
+
+const videos = require.context('../videos', true)
+const videoPath = (name) => images(videos, true)
